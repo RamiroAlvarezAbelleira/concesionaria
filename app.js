a78f2ca71e5b77d1a61c06743f42779e225bade0
Ramiro Alvarez <alvarezramiro1997@gmail.com>
2022-06-06 16:09:09 -0300

    agregue el archivo autos que contiene el array de autos de la concesionaria y ademas el archivo app que requiere el array de autos e implementa la funcionalidad de buscar un auto y marcarlo como vendido

---
+++ app.js
@@ -0,0 +1,21 @@
+let autos = require('./autos');
+let concesionaria = {
+    autos: autos,
+ 
+    buscarAuto: function(patente) {
+        for (let i = 0; i < this.autos.length; i++) {
+            if (this.autos[i].patente == patente) {
+                return this.autos[i];
+            }
+        }
+        return null;
+    },
+    
+    venderAuto: function(patente) {
+        let autoBuscado = this.buscarAuto(patente)
+        if (autoBuscado != null && autoBuscado.vendido ==          false) {
+            return autoBuscado.vendido = true;
+        }
+        return null;
+    }
+}
